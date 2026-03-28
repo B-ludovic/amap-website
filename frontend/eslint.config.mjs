fc10cdfd0f606d41fc93fe390b2f1e9d33ab2de4
B-ludovic <timboo974@gmail.com>
2026-03-28 17:42:55 +0100

    reverse: eslint nextVitals not iterable sur le build

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,12 +1,16 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
 
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
-export default [
-  ...compat.extends('next/core-web-vitals'),
-];
+export default eslintConfig;
