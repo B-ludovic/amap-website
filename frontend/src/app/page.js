f3ae917724d599c6029002f130ff7ebddfeb0c6c
B-ludovic <timboo974@gmail.com>
2026-08-16 18:25:16 +0200

    style: reverse des section info vers une teinte claire

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -124,7 +124,7 @@ export default async function HomePage() {
       </section>
 
       {/* Le cycle */}
-      <section className="band-forest">
+      <section className="band-sand">
         <div className="container cycle">
           <div className="cycle-head">
             <div>
